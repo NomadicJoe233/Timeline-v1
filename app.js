30da40a9c558c73fbd047e5d3759e4e0ec38273d
NomadicJoe233 <108058319+NomadicJoe233@users.noreply.github.com>
2022-06-24 21:46:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    let newVar;
+    let date = new Date();
+    let day = date.getDay();
+    if(day==0 || day==5) newVar = "Weekend";
+    res.render('list', {varName:newVar});
+})
+
+
+app.listen(3000); 
